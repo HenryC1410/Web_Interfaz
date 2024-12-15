44e6f2049ea9b52e8bbd28e42ccc66002b4ce2d2
HenryC1410 <henrygerman37@gmail.com>
2024-12-14 20:35:51 -0500

    2.3

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -33,3 +33,52 @@ document.addEventListener("DOMContentLoaded", () => {
     // Verificar estado inicial
     checkInputs();
 });
+
+
+const profileIcon = document.getElementById('profile-icon');
+const profileMenu = document.getElementById('profile-menu');
+
+
+profileIcon.addEventListener('click', (event) => {
+    event.preventDefault(); 
+    profileMenu.classList.toggle('show');
+});
+
+
+document.addEventListener('click', (event) => {
+    if (!profileIcon.contains(event.target) && !profileMenu.contains(event.target)) {
+        profileMenu.classList.remove('show');
+    }
+});
+
+
+
+const logoutOverlay = document.getElementById('logout-overlay');
+const overlayContent = document.querySelector('.overlay-content');
+const confirmLogout = document.getElementById('confirm-logout');
+const cancelLogout = document.getElementById('cancel-logout');
+const logoutOption = document.querySelector('.dropdown-menu li:nth-child(2) a');
+
+logoutOption.addEventListener('click', (event) => {
+    event.preventDefault(); 
+    logoutOverlay.style.display = 'flex'; 
+});
+
+
+cancelLogout.addEventListener('click', () => {
+    logoutOverlay.style.display = 'none'; 
+});
+
+
+confirmLogout.addEventListener('click', () => {
+
+    window.location.href = '../index.html'; 
+});
+
+
+logoutOverlay.addEventListener('click', (event) => {
+    if (!overlayContent.contains(event.target)) {
+        logoutOverlay.style.display = 'none'; 
+    }
+});
+
